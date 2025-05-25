c37f5ae5063a3ae0d4f94f0d468aecf752e1e90f
MBashov <galacticos.mb@gmail.com>
2025-05-25 19:18:21 +0200

    Task 08

---
+++ 02-Advanced Data Types/08.ts
@@ -0,0 +1,42 @@
+type TaskStatus = 'Logged' | 'Started' | 'InProgress' | 'Done';
+type UserType = { username: string, signupDate: Date }
+
+function assignTask(
+    user: UserType,
+    task: {
+        status: TaskStatus,
+        title: string,
+        daysRequired: number,
+        assignedTo:  UserType | undefined,
+        changeStatus(newStatus: TaskStatus): void
+    }
+) {
+    if (task.assignedTo == undefined) {
+        task.assignedTo = user;
+        console.log(`User ${user.username} assigned to task '${task.title}'`);
+    }
+}
+
+let user = {
+    username: 'Margaret',
+    signupDate: new Date(2022, 1, 13),
+    passwordHash: 'random'
+}
+let task1 = {
+    status: <'Logged' | 'Started' | 'InProgress' | 'Done'>'Logged',
+    title: 'Need assistance',
+    daysRequired: 1,
+    assignedTo: undefined,
+    changeStatus(newStatus: 'Logged' | 'Started' | 'InProgress' | 'Done') { this.status = newStatus; }
+}
+let task2 = {
+    status: <'Logged' | 'Started' | 'InProgress' | 'Done'>'Done',
+    title: 'Test',
+    daysRequired: 12,
+    assignedTo: undefined,
+    changeStatus(newStatus: 'Logged' | 'Started' | 'InProgress' | 'Done') { this.status = newStatus; },
+    moreProps: 300,
+    evenMore: 'wow'
+}
+assignTask(user, task1);
+assignTask(user, task2);
